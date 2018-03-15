482c692895d4476e7402a01db4b8db6972dfa01b
Zach <ssb.pub.key.is@ZqH7Mctu/7DNInxuwl12ECjfrAKUX2tBLq1rOldNhg0=.ed25519>
2018-03-15 15:13:40 +1300

    added submission panel

--- spirit.js
+++ spirit.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', displayFuture)
 
+//An array of future visions, provided by site visitors
 var ourFuture = [
   "the future is magic",
   "the future is solarpunk",
@@ -11,15 +12,23 @@ var ourFuture = [
 var len = ourFuture.length;
 var i = 0;
 
+//Display each future vision in the center of the page, wiht a 3 second space between each one.
 function displayFuture () {
   var future = document.getElementById('ourOptimisticFuture');
   future.innerHTML =  ourFuture[i]
   if(++i < len) {
     setTimeout(displayFuture, 3000)
   }
+  //When you've run through the list, reset and start again (this is how we loop forever)
   else {
     i = 0
     setTimeout(displayFuture, 3000)
   }
+}
+
+//Hide Submission Box when the submission is clicked
+function success () {
+  var submitBox = document.getElementById('vision-submission')
+  submitBox.classList.toggle('hidden')
 }
 
